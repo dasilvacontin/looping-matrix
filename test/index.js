08144f6af5761fb3278f2233fba71bb2ac099346
David da Silva <dasilvacontin@gmail.com>
2015-10-29 11:00:25 +0100

    Add test asserting clone integrity
    Check if modifying the matrix does not also modify its clones.

--- test/index.js
+++ test/index.js
@@ -7,5 +7,9 @@ test('awesomeness', function (t) {
   var loopingMatrix = new LoopingMatrix(50, 50)
   loopingMatrix.setCell(1, 1, 'cake')
   t.equal(loopingMatrix.getCell(51, 51), 'cake')
+
+  var copy = loopingMatrix.clone()
+  loopingMatrix.setCell(1, 1, 'lie')
+  t.equal(copy.getCell(1, 1), 'cake')
   t.end()
 })
